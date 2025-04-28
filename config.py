e92d7b647eed10aa8e71098dcd06404a9599c8b9
santyarena1 <santyarena01@gmail.com>
2025-04-27 23:45:56 -0300

    Configuración de base de datos dinámica para Render

--- config.py
+++ config.py
@@ -1,7 +1,24 @@
-DB_CONFIG = {
-    "dbname": "preciosdb",
-    "user": "postgres",
-    "password": "admin123",
-    "host": "localhost",
-    "port": "5432"
-}
+import os
+import urllib.parse
+
+# Si existe DATABASE_URL (entorno de producción como Render), usamos eso
+if "DATABASE_URL" in os.environ:
+    urllib.parse.uses_netloc.append("postgres")
+    url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
+
+    DB_CONFIG = {
+        "dbname": url.path[1:],
+        "user": url.username,
+        "password": url.password,
+        "host": url.hostname,
+        "port": url.port
+    }
+else:
+    # Local (tu PC)
+    DB_CONFIG = {
+        "dbname": "preciosdb",
+        "user": "postgres",
+        "password": "admin123",
+        "host": "localhost",
+        "port": "5432"
+    }
